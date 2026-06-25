1ff99793beabc68d073affb18acc522ff42467ae
Andrew Velez <andrewvelez@outlook.com>
2026-06-25 10:12:49 -0400

    removing old docs, project boilerplate,  updatin gfile names

--- docs/linkup-p2p-chat/public/static/app.js
+++
@@ -1,9 +0,0 @@
-/**
- * Browser entrypoint. Imports components for their custom-element registration.
- * by: Andrew Velez
- */
-
-import "./components/connection-panel.js";
-import "./components/message-list.js";
-import "./components/message-composer.js";
-import "./components/link-up-chat.js";
--- docs/linkup/build.js
+++
@@ -1,6 +0,0 @@
-import { $ } from "bun";
-
-await $`mkdir -p dist`;
-await $`cp -r web dist/web`;
-
-console.log("build complete");
